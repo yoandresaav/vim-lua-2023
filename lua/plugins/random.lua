b35502403ad7a8aa6e47f0fe1286410396f5da54
Yoandre Saavedra Gonzalez <2855358+yoandresaav@users.noreply.github.com>
2024-04-08 10:06:09 -0600

    fix: formatting entirly files

--- lua/plugins/random.lua
+++ lua/plugins/random.lua
@@ -89,6 +89,13 @@ return {
 			{ "Pheon-Dev/pigeon", opts = {} },
 		},
 		event = "VeryLazy",
+		requires = {
+			"kyazdani42/nvim-web-devicons", opt = true,
+		},
+		use {
+			'nvim-telescope/telescope.nvim', tag = '0.1.0'.
+			requires = {{ 'nvim-lua/plenary.nvim' }}
+		}
 	},
 
 	"nvim-tree/nvim-web-devicons",
